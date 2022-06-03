d3d29ad933537532fa41b620c8be04be467fe023
João Helis Bernardo <joaohelis.bernardo@gmail.com>
2022-06-02 21:31:29 -0300

    [New feature] Creating task

--- task_manager/manager.py
+++ task_manager/manager.py
@@ -1,25 +1,47 @@
-from fastapi import FastAPI
+from enum import Enum
+from lib2to3.pytree import Base
+from pydoc import describe
+from uuid import UUID, uuid4
+
+from fastapi import FastAPI, status
+from pydantic import BaseModel, constr
+
+
+class StatusEnum(str, Enum):
+    finished = "finished"
+    not_finished = "not finished"
+
+
+class TaskEntry(BaseModel):
+    title: constr(min_length=3, max_length=50)
+    description: constr(max_length=140)
+    status: StatusEnum = StatusEnum.not_finished
+
+
+class Task(TaskEntry):
+    id: UUID
+
 
 app = FastAPI()
 
 TASKS = [
     {
         "id": "1",
-        "titulo": "fazer compras",
-        "descrição": "comprar leite e ovos",
-        "estado": "não finalizado",
+        "title": "fazer compras",
+        "description": "comprar leite e ovos",
+        "status": "não finalizado",
     },
     {
         "id": "2",
-        "titulo": "levar o cachorro para tosar",
-        "descrição": "está muito peludo",
-        "estado": "não finalizado",
+        "title": "levar o cachorro para tosar",
+        "description": "está muito peludo",
+        "status": "não finalizado",
     },
     {
         "id": "3",
-        "titulo": "lavar roupas",
-        "descrição": "estão sujas",
-        "estado": "não finalizado",
+        "title": "lavar roupas",
+        "description": "estão sujas",
+        "status": "não finalizado",
     },
 ]
 
@@ -27,3 +49,11 @@ TASKS = [
 @app.get("/tasks")
 def list():
     return TASKS
+
+
+@app.post("/tasks", response_model=Task, status_code=status.HTTP_201_CREATED)
+def create(task: TaskEntry):
+    new_task = task.dict()
+    new_task.update({"id": uuid4()})
+    TASKS.append(new_task)
+    return new_task
--- tests/test_manager.py
+++ tests/test_manager.py
@@ -1,3 +1,6 @@
+from http import client
+from urllib import response
+
 from fastapi import status
 from fastapi.testclient import TestClient
 
@@ -64,4 +67,83 @@ def test_when_listing_tasks_a_unique_task_should_have_a_status():
     client = TestClient(app)
     response = client.get("/tasks")
     assert "status" in response.json().pop()
+    TASKS.clear()
+
+
+def test_tasks_resource_should_accept_http_post():
+    client = TestClient(app)
+    response = client.post("/tasks")
+    assert response.status_code != status.HTTP_405_METHOD_NOT_ALLOWED
+
+
+def test_task_creation_must_have_title():
+    client = TestClient(app)
+    response = client.post("/tasks", json={})
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+def test_task_creation_must_contain_between_3_and_50_characters():
+    client = TestClient(app)
+    response = client.post("/tasks", json={"title": 2 * "*"})
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    response = client.post("/tasks", json={"title": 51 * "*"})
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+def test_task_creation_must_have_a_description():
+    client = TestClient(app)
+    response = client.post("/tasks", json={"title": "title"})
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+def test_task_creation_must_have_a_description_with_max_lenght_of_140_characters():
+    client = TestClient(app)
+    response = client.post(
+        "/tasks", json={"title": "title", "description": "*" * 141}
+    )
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+def test_when_creating_a_test_it_must_be_returned_as_response():
+    client = TestClient(app)
+    task = {"title": "title", "description": "description"}
+    response = client.post("/tasks", json=task)
+    created_task = response.json()
+    print(created_task)
+    assert task["title"] == created_task["title"]
+    assert task["description"] == created_task["description"]
+    TASKS.clear()
+
+
+def test_when_creating_a_task_it_must_have_a_unique_identifier():
+    client = TestClient(app)
+    task_1 = {"title": "title 1", "description": "description 1"}
+    task_2 = {"title": "title 2", "description": "description 2"}
+    response_1 = client.post("/tasks", json=task_1)
+    response_2 = client.post("/tasks", json=task_2)
+    assert response_1.json()["id"] != response_2.json()["id"]
+    TASKS.clear()
+
+
+def test_task_must_have_a_state_with_not_finished_value_as_default():
+    client = TestClient(app)
+    task = {"title": "title", "description": "description"}
+    response = client.post("/tasks", json=task)
+    assert response.json()["status"] == "not finished"
+    TASKS.clear()
+
+
+def test_when_creating_a_task_the_status_code_must_be_201():
+    client = TestClient(app)
+    task = {"title": "title", "description": "description"}
+    response = client.post("/tasks", json=task)
+    assert response.status_code == status.HTTP_201_CREATED
+    TASKS.clear()
+
+
+def test_when_creating_a_task_it_must_be_persisted():
+    client = TestClient(app)
+    task = {"title": "title", "description": "description"}
+    response = client.post("/tasks", json=task)
+    assert len(TASKS) == 1
     TASKS.clear()
